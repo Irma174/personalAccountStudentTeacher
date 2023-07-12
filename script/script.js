33cb09b4e1bac7e19a364a18eeab7d31ac74f983
Irma174 <averenkova80@mail.ru>
2023-07-13 03:41:11 +0500

    Попап при нажатии на кнопку 'Информация'

--- script/script.js
+++ script/script.js
@@ -1,33 +1,9 @@
-let btnMyStudents = document.querySelector('.vvv');
-let btnMyStydentsInformation = document.querySelector('.mystudents__information-container');
-
-// function promtEnter(event){   
-//    prompt('Чтобы получать индивидуальные предложения укажите дату вашего рождения:');
-//     event.preventDefault();
-// }
-
-
-btnMyStudents.addEventListener('click',  () => {
-        alert('Метод для вывода сообщения в веб-консоль.');
-        btnMyStydentsInformation.classList.add
-})
-
-
-// const linkPromotion = document.querySelector('.birthday_data');
-//     function promtEnter(event){   
-//     const birthday = prompt('Чтобы получать индивидуальные предложения укажите дату вашего рождения:');
-//     linkPromotion.textContent = birthday;
-
-//     event.preventDefault();
-// }
-// linkPromotion.addEventListener('click', promtEnter);
-
-btnMyStudents.forEach((button) => { // Перебираем все кнопки
-    button.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
-        e.preventDefault(); // Предотвращаем дефолтное поведение браузера
-
-        alert('Метод для вывода сообщения в веб-консоль.');
-        // popupBg.classList.add('active'); // Добавляем класс 'active' для фона
-        // popup.classList.add('active'); // И для самого окна
-    })
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".modal");
+  var instances = M.Modal.init(elems, {
+    opacity: 0.2,
+    inDuration: 400,
+    outDuration: 400,
+    preventScrolling: false,
+  });
 });
