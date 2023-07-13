3c3b9827636a9fd8025f6360a6743dfc78b811bc
Irma174 <averenkova80@mail.ru>
2023-07-13 18:53:43 +0500

    Слайдер

--- script/script.js
+++ script/script.js
@@ -1,10 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var elems = document.querySelectorAll(".modal");
-  var instances = M.Modal.init(elems, {
+  var popup = document.querySelectorAll(".modal");
+  var instances = M.Modal.init(popup, {
     opacity: 0.2,
     inDuration: 400,
     outDuration: 400,
     preventScrolling: false,
+  });
+
+  var elems = document.querySelectorAll('.carousel');
+  var inst = M.Carousel.init(elems,{
+    duration: 300,
+    dist: 0,
+    shift: 20,
+    numVisible: 1,
+    fullWidht: true
   });
 });
 
